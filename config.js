5c916139e698f6f56922e92f020e089a32d44c7e
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2024-03-13 00:44:53 +0300

    Change config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  // API_ENDPOINT: 'http://ec2-13-51-162-63.eu-north-1.compute.amazonaws.com/',
-  API_ENDPOINT: 'http://localhost:5000/',
+  API_ENDPOINT: 'http://ec2-13-51-162-63.eu-north-1.compute.amazonaws.com/',
+  // API_ENDPOINT: 'http://localhost:5000/',
 };
 
 export default config;
