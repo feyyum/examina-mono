f36fe4a85d784fbd0c13f43b709dcdec389019ee
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2024-04-02 03:27:42 +0300

    Change prod backend url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  // API_ENDPOINT: 'https://api.examina.space/',
-  API_ENDPOINT: 'http://localhost:8181/',
+  API_ENDPOINT: 'https://api.examina.space/',
+  // API_ENDPOINT: 'http://localhost:8181/',
 };
 
 export default config;
