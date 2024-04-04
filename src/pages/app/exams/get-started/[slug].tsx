daa652e44977ca75140b28c72271ac9cb5aa3b5f
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2024-04-04 20:44:13 +0300

    Update get-started page

--- src/pages/app/exams/get-started/[slug].tsx
+++ src/pages/app/exams/get-started/[slug].tsx
@@ -5,33 +5,107 @@ import { useQuery } from '@tanstack/react-query';
 import { getExamDetails } from '@/lib/Client/Exam';
 import { humanize } from '../../../../../utils/formatter';
 
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../../store';
+
 // Custom Layout
 import Layout from '../layout';
 
 // Icons
 import ExamIcon from '@/icons/globe.svg';
 import Clock from '@/icons/clock.svg';
+import Choz from '@/icons/choz.svg';
 
 function ExamDetail() {
   const router = useRouter();
   const examID: string = router.query.slug as string;
 
-  console.log('examID', examID);
+  const account = useSelector((state: RootState) => state.account);
 
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError, refetch } = useQuery({
     queryKey: ['exam'],
     queryFn: () => getExamDetails(examID),
     enabled: !!examID, // Only fetch data when examID is available
   });
 
-  console.log(data);
-
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <Layout>
+        <div className={styles.container}>
+          <div className={styles.content_container}>
+            <div className={styles.card_container}>
+              <div className={styles.card_inner_container}>
+                <div className={styles.error_container}>
+                  <h3 className={styles.try_again_title}>Are you bored? &#129393;</h3>
+                  <p className={styles.try_again_desc}>
+                    We are working hard to get the test details. Please wait a moment.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.footer_container}`}>
+            <div className={styles.scale__container}>
+              <Image src={Choz} alt="" />
+              <div className={styles.footer_nav_container}>
+                <a className={styles.footer_nav_item} href="#">
+                  Overview
+                </a>
+                <a className={styles.footer_nav_item} href="#">
+                  Blog
+                </a>
+                <a className={styles.footer_nav_item} href="#">
+                  Docs
+                </a>
+              </div>
+              <p className={styles.footer_copyright}>© 2024 Examina</p>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
   }
 
-  if (data === undefined) {
-    return <div>Not found</div>;
+  if (data === undefined && isError) {
+    return (
+      <Layout>
+        <div className={styles.container}>
+          <div className={styles.content_container}>
+            <div className={styles.card_container}>
+              <div className={styles.card_inner_container}>
+                <div className={styles.error_container}>
+                  <h3 className={styles.try_again_title}>Something went wrong &#128553;</h3>
+                  <p className={styles.try_again_desc}>
+                    Probably this test is invalid or outdated. But you may want to try your luck
+                    again. Please click this magic button for that.
+                  </p>
+                  <p className={styles.try_again_button} onClick={() => refetch()}>
+                    TRY AGAIN
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.footer_container}`}>
+            <div className={styles.scale__container}>
+              <Image src={Choz} alt="" />
+              <div className={styles.footer_nav_container}>
+                <a className={styles.footer_nav_item} href="#">
+                  Overview
+                </a>
+                <a className={styles.footer_nav_item} href="#">
+                  Blog
+                </a>
+                <a className={styles.footer_nav_item} href="#">
+                  Docs
+                </a>
+              </div>
+              <p className={styles.footer_copyright}>© 2024 Examina</p>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
   }
 
   return (
@@ -77,21 +151,37 @@ function ExamDetail() {
               </div>
               <div className={styles.connect_container}>
                 <div className={styles.connect_button_container}>
-                  <p className={styles.connect_button_text}>Connect wallet</p>
+                  <p className={styles.connect_button_text}>
+                    {account.wallets[0] ? 'Start Exam' : 'Connect Wallet'}
+                  </p>
                 </div>
-                <p className={styles.connect_container_desc}>
-                  You must have an Auro Wallet account before using it. Not there yet?{' '}
-                  <a href="#">Create now!</a>
-                </p>
+                {account.wallets[0] ? (
+                  <p className={styles.connect_container_desc_connected}>
+                    You are using this wallet address:{' '}
+                    <a
+                      href={`https://minascan.io/mainnet/account/${account.wallets[0]}/`}
+                      target="_blank"
+                    >
+                      {account.wallets[0].slice(0, 4)}...
+                      {account.wallets[0].slice(
+                        account.wallets[0].length - 4,
+                        account.wallets[0].length
+                      )}
+                    </a>
+                  </p>
+                ) : (
+                  <p className={styles.connect_container_desc}>
+                    You must have an Auro Wallet account before using it. Not there yet?{' '}
+                    <a href="#">Create now!</a>
+                  </p>
+                )}
               </div>
             </div>
           </div>
         </div>
         <div className={`${styles.footer_container}`}>
           <div className={styles.scale__container}>
-            <h3 className={styles.footer_logo}>
-              exa<span>mina</span>
-            </h3>
+            <Image src={Choz} alt="" />
             <div className={styles.footer_nav_container}>
               <a className={styles.footer_nav_item} href="#">
                 Overview
