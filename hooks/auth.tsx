ef4a0cc7e5eed6c8e7b266e3514e61236148624f
Mert Akyazi <32460871+BlocksOnChain@users.noreply.github.com>
2024-09-10 17:53:45 +0300

    hotfix: switchChain function changes on Auro Wallet implemented to fix connect wallet issue

--- hooks/auth.tsx
+++ hooks/auth.tsx
@@ -18,7 +18,7 @@ export async function switchChain(chainId: string) {
   }
 
   const chainRes = await mina.switchChain({
-    chainId: chainId,
+    networkID: chainId,
   });
 
   if (chainRes.chainId !== chainId) {
@@ -40,13 +40,13 @@ export async function connectWallet() {
     const accounts: string[] = await mina.getAccounts();
 
     if (accounts.length > 0) {
-      await switchChain('mainnet');
+      await switchChain('mina:mainnet');
       return accounts[0];
     }
 
     const publicKeyBase58: string = (await mina.requestAccounts())[0];
 
-    const chain = await switchChain('mainnet');
+    const chain = await switchChain('mina:mainnet');
 
     if (!chain) {
       throw new Error('Failed to switch chain. Please try again.');
